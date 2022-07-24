0e47ce029aeac943f5e054436ddbc6f1212bf56c
Erdem <erdemdoruk3@gmail.com>
2022-07-25 02:45:12 +0300

    Finish

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function getPassword(){
+    // We set a random password
     const chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz@#$%^&*( )_+><:{}[]'";
     let passwordLength = 16;
     let password = ' ';
@@ -7,35 +8,28 @@ function getPassword(){
     for(let i =0; i< passwordLength; i++){
         let randomNumber = Math.floor(Math.random() * chars.length);
         password += chars.substring(randomNumber, randomNumber + 1)
-        console.log(password)
     }
 
     document.getElementById("password").value = password
 }
 
 
-
 function copy() {
-
+    
+    // Copy password button
     let copyText = document.getElementById("password");
   
-
     copyText.select();
     copyText.setSelectionRange(0, 99999); 
   
-
     navigator.clipboard.writeText(copyText.value);
 
+    // For the copied text
     let copied = document.getElementById("copied");
-
     copied.style.display = "flex"
-
 
     setTimeout(function(){
         copied.style.display = "none"
       }, 1000);
   
   }
-
-
-
